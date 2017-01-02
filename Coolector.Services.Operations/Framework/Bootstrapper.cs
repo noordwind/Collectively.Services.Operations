e765ee658f285805a84d2531864eb47ff3eb6bd2
spetz <spetz90@gmail.com>
2017-01-02 10:49:44 +0100

    Custom Nancy JSON serializer, RabbitMQ container with connection retry attempts.

--- Coolector.Services.Operations/Framework/Bootstrapper.cs
+++ Coolector.Services.Operations/Framework/Bootstrapper.cs
@@ -8,19 +8,20 @@ using Coolector.Common.Events;
 using Coolector.Common.Exceptionless;
 using Coolector.Common.Mongo;
 using Coolector.Common.Nancy;
+using Coolector.Common.Nancy.Serialization;
 using Coolector.Common.Extensions;
+using Coolector.Common.RabbitMq;
 using Coolector.Common.Services;
 using Coolector.Services.Operations.Repositories;
 using Coolector.Services.Operations.Services;
+using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
 using Nancy;
 using Nancy.Bootstrapper;
 using NLog;
 using Polly;
 using RabbitMQ.Client.Exceptions;
-using RawRabbit;
 using RawRabbit.Configuration;
-using RawRabbit.vNext;
 
 namespace Coolector.Services.Operations.Framework
 {
@@ -42,31 +43,16 @@ namespace Coolector.Services.Operations.Framework
             Logger.Info("Coolector.Services.Storage Configuring application container");
             base.ConfigureApplicationContainer(container);
 
-            var rmqRetryPolicy = Policy
-                .Handle<ConnectFailureException>()
-                .Or<BrokerUnreachableException>()
-                .Or<IOException>()
-                .WaitAndRetry(5, retryAttempt =>
-                            TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
-                    (exception, timeSpan, retryCount, context) => {
-                        Logger.Error(exception, $"Cannot connect to RabbitMQ. retryCount:{retryCount}, duration:{timeSpan}");
-                    }
-                );
-
             container.Update(builder =>
             {
+                builder.RegisterType<CustomJsonSerializer>().As<JsonSerializer>().SingleInstance();
                 builder.RegisterInstance(_configuration.GetSettings<MongoDbSettings>()).SingleInstance();
                 builder.RegisterInstance(AutoMapperConfig.InitializeMapper());
                 builder.RegisterModule<MongoDbModule>();
                 builder.RegisterType<MongoDbInitializer>().As<IDatabaseInitializer>();
                 builder.RegisterInstance(_configuration.GetSettings<ExceptionlessSettings>()).SingleInstance();
                 builder.RegisterType<ExceptionlessExceptionHandler>().As<IExceptionHandler>().SingleInstance();
-                var rawRabbitConfiguration = _configuration.GetSettings<RawRabbitConfiguration>();
-                builder.RegisterInstance(rawRabbitConfiguration).SingleInstance();
-                rmqRetryPolicy.Execute(() => builder
-                        .RegisterInstance(BusClientFactory.CreateDefault(rawRabbitConfiguration))
-                        .As<IBusClient>()
-                );
+                RabbitMqContainer.Register(builder, _configuration.GetSettings<RawRabbitConfiguration>());
                 builder.RegisterType<OperationRepository>().As<IOperationRepository>();
                 builder.RegisterType<OperationService>().As<IOperationService>();
                 var assembly = typeof(Startup).GetTypeInfo().Assembly;
